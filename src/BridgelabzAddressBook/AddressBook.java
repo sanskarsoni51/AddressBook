38466af91f27b11e2b59950980f00fe7c99c314f
Sanskar soni <sanskarsoni.cse23@jecrc.ac.in>
2023-11-23 16:16:02 +0530

    initial commit

---
+++ src/BridgelabzAddressBook/AddressBook.java
@@ -0,0 +1,9 @@
+package BridgelabzAddressBook;
+
+public class AddressBook {
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Address Book Java Functionality ");
+
+    }
+}
